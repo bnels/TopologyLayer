24641a292de41858d8c109e1b42115f7f103b626
Brad Nelson <bjnelson@stanford.edu>
2019-06-08 09:53:21 -0700

    sparsevec opterations to use iterators

--- topologylayer/functional/persistence/sparsevec.h
+++ topologylayer/functional/persistence/sparsevec.h
@@ -33,7 +33,9 @@ class SparseF2Vec{
 			// TODO: sort indices?
 		}
 
+
 		// add two vectors over F2
+		// IMPORTANT: assumes that nzinds is sorted
 		void add(const SparseF2Vec<T> &x) {
 			// quick check to see if there is anything to do
 			if (x.nzinds.size() == 0) {return;}
@@ -41,36 +43,34 @@ class SparseF2Vec{
 
 			// now we know there is something non-trivial to do
 			std::vector<T> tmp;
-			size_t i1 = 0;
-			size_t i2 = 0;
+			auto i1 = nzinds.cbegin();
+			auto i2 = x.nzinds.cbegin();
 			do {
-				T v1 = nzinds[i1];
-				T v2 = x.nzinds[i2];
-				if (v1 == v2) {
-					// F2 means sum is 0
-					i1++;
-					i2++;
-				} else if (v1 < v2) {
-					tmp.push_back(v1);
-					i1++;
-				} else { // v2 < v1
-					tmp.push_back(v2);
-					i2++;
+				if (*i1 == *i2) {
+					++i1;
+					++i2;
+				} else if (*i1 < *i2) {
+					tmp.push_back(*i1);
+					++i1;
+				} else {
+					tmp.push_back(*i2);
+					++i2;
 				}
-			} while (i1 < nzinds.size() && i2 < x.nzinds.size());
+			} while (i1 < nzinds.cend() && i2 < x.nzinds.cend());
 			// run through rest of entries and dump in
-			// only one of the loops will actually do anything
-			while (i1 < nzinds.size()) {
-				tmp.push_back(nzinds[i1]);
-				i1++;
+			// at most one of the loops does anything
+			while (i1 < nzinds.cend()) {
+				tmp.push_back(*i1);
+				++i1;
 			}
-			while (i2 < x.nzinds.size()) {
-				tmp.push_back(x.nzinds[i2]);
-				i2++;
+			while (i2 < x.nzinds.cend()) {
+				tmp.push_back(*i2);
+				++i2;
 			}
 			nzinds = tmp;
 			return;
 		}
+
 
 		// dot product of two vectors
 		// IMPORTANT: this function assumes that nzinds are sorted!
@@ -79,22 +79,20 @@ class SparseF2Vec{
 			// quick check to see if anything to be done
 			if (nzinds.size() == 0 || x.nzinds.size() == 0) return 0;
 			// loop over indices to compute size of intersection
-			size_t i1 = 0;
-			size_t i2 = 0;
+			auto i1 = nzinds.cbegin();
+			auto i2 = x.nzinds.cbegin();
 			size_t intersection = 0;
 			do {
-				T v1 = nzinds[i1];
-				T v2 = x.nzinds[i2];
-				if (v1 == v2) {
-					i1++;
-					i2++;
-					intersection++;
-				} else if (v1 < v2) {
-					i1++;
+				if (*i1 == *i2) {
+					++i1;
+					++i2;
+					++intersection;
+				} else if (*i1 < *i2) {
+					++i1;
 				} else { // v2 < v1
-					i2++;
+					++i2;
 				}
-			} while (i1 < nzinds.size() && i2 < x.nzinds.size());
+			} while (i1 < nzinds.cend() && i2 < x.nzinds.cend());
 			// mod-2
 			return intersection % 2;
 		}
